8b82eb24046d383e1209aa1f78d7786b2ef7c367
mindyls <mindyls@users.noreply.github.com>
2023-05-16 13:29:11 -0700

    Update init files

--- geoips_plugin_example/plugins/modules/colormaps/visir/__init__.py
+++ geoips_plugin_example/plugins/modules/colormaps/visir/__init__.py
@@ -9,8 +9,4 @@
 # # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
-"""Init file for geoips_plugin_example."""
-
-from .version import __version__, __version_tuple__
-
-__all__ = ["__version__", "__version_tuple__"]
+"""Init file for geoips_plugin_example/colormaps/visir."""
