54c54ffbfc00390c84038c678d28c820a2987713
mindyls <13873624+mindyls@users.noreply.github.com>
2026-01-14 22:28:48 +0000

    1.17.2 release NRLMMD-GEOIPS/geoips#1170

--- tests/scripts/abi_aws.sh
+++ tests/scripts/abi_aws.sh
@@ -3,16 +3,15 @@
 
 #!/bin/bash
 
-run_procflow $GEOIPS_TESTDATA_DIR/test_data_noaa_aws/data/goes16/20200918/1950/* \
-             --procflow single_source \
-             --reader_name abi_netcdf \
-             --resampled_read \
-             --product_name Infrared-Test \
-             --compare_path "$GEOIPS_PACKAGES_DIR/geoips_plugin_example/tests/outputs/abi_aws/<product>_image" \
-             --output_formatter imagery_test \
-             --filename_formatter test_fname \
-             --sector_list goes_east \
-
+geoips run single_source \
+    $GEOIPS_TESTDATA_DIR/test_data_abi/data/goes16_20200918_1950/* \
+    --reader_name abi_netcdf \
+    --resampled_read \
+    --product_name Infrared-Test \
+    --compare_path "$GEOIPS_PACKAGES_DIR/geoips_plugin_example/tests/outputs/abi_aws/<product>_image" \
+    --output_formatter imagery_test \
+    --filename_formatter test_fname \
+    --sector_list goes_east
 retval=$?
 
 exit $retval
